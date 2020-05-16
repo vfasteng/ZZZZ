41e7c6090e3efc62c6f434a9aeda9d407cfdf872
vfasteng <55648806+vfasteng@users.noreply.github.com>
2020-05-16 07:11:50 +0200

    Add files via upload

---
+++ ammo-constraint.js
@@ -0,0 +1,15 @@
+AFRAME.registerComponent('ammo-constraint', {
+  schema: {
+    foo: {type: 'string'}
+  },
+
+  init: function () {
+    var el = this.el;
+    var data = this.data;
+  },
+
+  update: function () {
+    var el = this.el;
+    var data = this.data;
+  }
+});
